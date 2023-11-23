371856bdce90185362d5c81e7f33f313f8d459b5
Kyosukyuu <82899136+Kyosukyuu@users.noreply.github.com>
2023-11-23 13:31:42 -0500

    add window switching while in terminal mode

--- mappings.lua
+++ mappings.lua
@@ -15,6 +15,10 @@ M.general = {
   },
   t = {
     ["<ESC>"] = { "<C-\\><C-n>", "escape terminal" },
+    ["<C-w>h"] = { "<Cmd>wincmd h<CR>", "Go to the left window" },
+    ["<C-w>j"] = { "<Cmd>wincmd j<CR>", "Go to the down window" },
+    ["<C-w>k"] = { "<Cmd>wincmd k<CR>", "Go to the up window" },
+    ["<C-w>l"] = { "<Cmd>wincmd l<CR>", "Go to the right window" },
   },
 }
 
